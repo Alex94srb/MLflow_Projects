7d6b10ab6946da47fd16ea58f7b265b8c590c679
Aleksandar <aleksandar.cvetanovic@thingsolver.com>
2021-10-15 12:11:06 +0200

    Again removed r_name variable

--- train_random_forest_reg.py
+++ train_random_forest_reg.py
@@ -49,8 +49,7 @@ def plot_graphs(x_data, y_data, x_label, y_label, title):
 
 @click.command()
 @click.option("--params--", type=dict, help="Dictionary with parameters for Random Forest Regressor")
-@click.option("--r-name", default="Lab-2:RF Petrol Regression Experiment - Projects", type=str, help="Name of the MLflow run")    
-def train_random_forest_reg(params, r_name):
+def train_random_forest_reg(params):
     """
     This method trains, computes metrics, and logs all metrics, parameters,
     and artifacts for the current run using the MLflow APIs
@@ -66,7 +65,7 @@ def train_random_forest_reg(params, r_name):
     df = pd.read_csv(petrol_cons)
 
     # <------------------- MLflow ------------------->
-    with mlflow.start_run(run_name=r_name) as run:
+    with mlflow.start_run() as run:
         # define the random forest regressor model
         rf = RandomForestRegressor(**params)
 
